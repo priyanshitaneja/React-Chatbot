fd843773034536ee95db2f2b82add0b50abbb577
Vivek Vaibhav <vivek@oriserve.com>
2020-05-06 20:15:22 +0530

    scroll to view area

--- src/popDiv.jsx
+++ src/popDiv.jsx
@@ -1,30 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./styles.css";
 import UserMsg from "./userMsg";
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 
 
-const botMessage=[
-  {id: 1, text: "Hi, How can I help you today?"}, 
-  {id:2, text: "Are you looking for?"},
-  {id:3, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "},
-  {id:4, text: "abcdefghijklmnopqrstuvwxyz"},
-  {id:5, text: "I am your virtual help bot"},
-  {id:6, text: "Want to connect to an agent"},
-  {id:7, text: "lorem ipsum 5"},
-  {id:8, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "},
-  {id:9, text: "Any random text zhxbikhsxokhnkxhnkiaqbxkqbakxhbqklxhakbhxloa"}
+const botMessage = [
+  { id: 1, text: "Hi, How can I help you today?" },
+  { id: 2, text: "Are you looking for?" },
+  { id: 3, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " },
+  { id: 4, text: "abcdefghijklmnopqrstuvwxyz" },
+  { id: 5, text: "I am your virtual help bot" },
+  { id: 6, text: "Want to connect to an agent" },
+  { id: 7, text: "lorem ipsum 5" },
+  { id: 8, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " },
+  { id: 9, text: "Any random text zhxbikhsxokhnkxhnkiaqbxkqbakxhbqklxhakbhxloa" }
 ];
 
 // const userMsg = [];
 
-function PopDiv () {
+function PopDiv() {
 
-  const [close, closeBot]= useState(false);
+  const [close, closeBot] = useState(false);
 
   const [inputText, setInputText] = useState("");
-  const [msg, sendMsg]=useState([]);
+  const [msg, sendMsg] = useState([]);
+
+  const chatbodyRef = useRef()
 
   function handleClick() {
     closeBot(true);
@@ -36,60 +38,64 @@ function PopDiv () {
   }
 
   function handleSend() {
+    chatbodyRef.current.scrollIntoView({ behavior: 'smooth' })
     sendMsg(prevMsg => {
       return [...prevMsg, inputText];
     });
     setInputText("");
+    
   }
 
   // const classes = useStyles();
 
   return (
-    <div className="BotWindow" style={{ display : close ? "none" : null}}>
-      
-    <div class="botHeader">
-     <HighlightOffIcon 
-        className="closeIcon"
-        onClick= {handleClick}
+    <div className="BotWindow" style={{ display: close ? "none" : null }}>
+
+      <div class="botHeader">
+        <HighlightOffIcon
+          className="closeIcon"
+          onClick={handleClick}
         />
-    </div>
+      </div>
 
 
-    <div class="chatArea">  
-      {botMessage.map(function(item){
-        return (
-            <p class="popUpMsg" key={item.id}>          
-             {item.text}
+      <div  class="chatArea">
+        {botMessage.map(function (item) {
+          return (
+            <p class="popUpMsg" key={item.id}>
+              {item.text}
             </p>
-        )
-      })};
+          )
+        })};
 
-      {msg.map(function(message){
-        return (
+      {msg.map(function (message) {
+          return (
             <UserMsg
-            text={message} />
-        )
-      })};
+              text={message} />
+          )
+        })};
 
+        <div ref={chatbodyRef} />
     </div>
-      
-    <div class="inputArea"> 
-      <input 
-        class="popUpInput" 
-        type="text" 
-        placeholder="Type your query here . . ." 
-        value={inputText}
-        onChange={handleChange}
+    
+
+      <div class="inputArea">
+        <input
+          class="popUpInput"
+          type="text"
+          placeholder="Type your query here . . ."
+          value={inputText}
+          onChange={handleChange}
         />
 
-      <button class="sendBtn" onClick={handleSend}>
-        Send
+        <button class="sendBtn" onClick={handleSend}>
+          Send
       </button>
-    </div>
-        
-      
+      </div>
 
-       {/* { close ? null : <PopDiv />}  */}
+
+
+      {/* { close ? null : <PopDiv />}  */}
 
     </div>
   )
