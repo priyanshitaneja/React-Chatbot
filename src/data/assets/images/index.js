ce7d0113e5f5f607713b3875a071d964dac8ed3f
priyanshitaneja <tanejapriyanshi1997@gmail.com>
2020-05-18 13:26:07 +0530

    customer-avatar

--- src/data/assets/images/index.js
+++ src/data/assets/images/index.js
@@ -1,3 +1,5 @@
 // export const bg = require("../images/bg.jpg");
 
 export const avatar = require("../images/botlogo.jpg");
+
+export const customerAvatar = require("../images/customerAvatar.png");
